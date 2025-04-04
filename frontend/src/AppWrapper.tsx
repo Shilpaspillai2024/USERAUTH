598d82df2c5ae7033ca013507fb503cdf729a5df
Shilpaspillai2024 <shilpaspillaitvla@gmail.com>
2025-04-04 09:39:28 +0530

    some minor issues

--- frontend/src/AppWrapper.tsx
+++ frontend/src/AppWrapper.tsx
@@ -1,12 +1,8 @@
-import { useLocation } from "react-router-dom";
 import App from "./App.tsx";
 import AdminApp from "./AdminApp.tsx";
 
 function AppWrapper() {
-  const location = useLocation(); 
-  const isAdminMode = location.pathname.startsWith("/admin"); 
-  console.log("Current Path:", location.pathname);
-  console.log("isAdminMode:", isAdminMode)
+  const isAdminMode = window.location.pathname.startsWith("/admin");
 
   return isAdminMode ? <AdminApp /> : <App />;
 }
