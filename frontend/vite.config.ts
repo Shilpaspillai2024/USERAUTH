018727cbc3881e03547b42ec24bd3df4df284ff6
Shilpaspillai2024 <shilpaspillaitvla@gmail.com>
2025-04-04 09:14:20 +0530

    small changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000, // Set the server to run on port 3000
-//   }
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -19,11 +9,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist', 
-  },
-  resolve: {
-    alias: {
-      '@': '/src', 
-    },
   }
 })
 
