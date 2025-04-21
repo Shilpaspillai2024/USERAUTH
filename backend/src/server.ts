9092badd4de65c4f3b14e8498267c1e9d30b6133
Shilpaspillai2024 <shilpaspillaitvla@gmail.com>
2025-04-21 18:22:10 +0530

    fix:cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,20 +15,10 @@ const app=express();
 
 
 
-
-const allowedOrigins = ['https://userauth-delta.vercel.app', 'http://localhost:3000'];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: "https://userauth-delta.vercel.app", 
+  credentials: true                
 }));
-
 
 
 app.use(express.json());
